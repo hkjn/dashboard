862825c3e66ad73ad27ad91431f1a058c820b905
Henrik Jonsson <me@hkjn.me>
2014-12-06 11:49:51 +0100

    s/C/cfg/g, to not give the impression that the config should be accessed externally (it loads from config.yaml, via hkjn.me/config)
    also makes the struct inline

--- dashboard.go
+++ dashboard.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	C             = Config{}
+	// Template for the alert email.
 	emailTemplate = `{{define "email"}}
 The probe <a href="http://fixme.com/#{{.Name}}">{{.Name}}</a> failed enough that this alert fired, as the arbitrary metric of 'badness' is {{.Badness}}, which we can all agree is a big number.<br/>
 The description of the probe is: &ldquo;{{.Desc}}&rdquo;<br/>
@@ -18,51 +18,51 @@ Failure details follow:<br/>
   <p>{{$r.Details}}</p>
 {{end}}
 {{end}}`
+	// Structure of config.yaml.
+	cfg = struct {
+		Whitelist []string
+		Sendgrid  struct {
+			User, Password string
+		}
+		Alerts struct {
+			Sender, Recipient string
+		}
+		Service struct {
+			Id, Secret string
+		}
+		WebProbes []struct {
+			Target, Want, Name string
+		}
+		DnsProbe struct {
+			Target  string
+			Records struct {
+				Cname string
+				A     []string
+				Mx    []struct {
+					Host string
+					Pref uint16
+				}
+				Ns  []string
+				Txt []string
+			}
+		}
+	}{}
 )
 
-type Config struct {
-	Whitelist []string
-	Sendgrid  struct {
-		User, Password string
-	}
-	Alerts struct {
-		Sender, Recipient string
-	}
-	Service struct {
-		Id, Secret string
-	}
-	WebProbes []struct {
-		Target, Want, Name string
-	}
-	DnsProbe struct {
-		Target  string
-		Records struct {
-			Cname string
-			A     []string
-			Mx    []struct {
-				Host string
-				Pref uint16
-			}
-			Ns  []string
-			Txt []string
-		}
-	}
-}
-
 func init() {
-	config.MustLoad(&C)
+	config.MustLoad(&cfg)
 
-	googleauth.SetCredentials(C.Service.Id, C.Service.Secret)
+	googleauth.SetCredentials(cfg.Service.Id, cfg.Service.Secret)
 	googleauth.SetGatingFunc(func(gplusId string) bool {
-		for _, id := range C.Whitelist {
+		for _, id := range cfg.Whitelist {
 			if gplusId == id {
 				return true
 			}
 		}
 		return false
 	})
-	probes.Config.Sendgrid = C.Sendgrid
+	probes.Config.Sendgrid = cfg.Sendgrid
 	probes.Config.Template = emailTemplate
-	probes.Config.Alert.Sender = C.Alerts.Sender
-	probes.Config.Alert.Recipient = C.Alerts.Recipient
+	probes.Config.Alert.Sender = cfg.Alerts.Sender
+	probes.Config.Alert.Recipient = cfg.Alerts.Recipient
 }
