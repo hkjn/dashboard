d6b5206863695acccb365667df924570c0f22763
Henrik Jonsson <me@hkjn.me>
2014-12-08 20:47:20 +0100

    adds a basic test of the dashboard

---
+++ dashboard_test.go
@@ -0,0 +1,41 @@
+// Tests for the dashboard package.
+package dashboard
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestNewRouter(t *testing.T) {
+	cases := []struct {
+		method         string
+		pattern        string
+		wantCode       int
+		wantInResponse string
+		auth           bool
+	}{
+		{"GET", "/", 200, "g-signin", true},
+		{"GET", "/", 200, "WebProber", false},
+	}
+
+	for i, tt := range cases {
+		*authDisabled = !tt.auth
+		router := NewRouter()
+
+		req, err := http.NewRequest(tt.method, tt.pattern, nil)
+		if err != nil {
+			t.Fatalf("[%d] failed to create %s %s request: %v\n", i, tt.method, tt.pattern, err)
+		}
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req)
+
+		if w.Code != tt.wantCode {
+			t.Fatalf("[%d] want HTTP response %d for %s %s, got %d\n", i, tt.wantCode, tt.method, tt.pattern, w.Code)
+		}
+		if !strings.Contains(w.Body.String(), tt.wantInResponse) {
+			t.Fatalf("[%d] want %q in response for %s %s, didn't get it: \n%s\n", i, tt.wantInResponse, tt.method, tt.pattern, w.Body.String())
+		}
+	}
+}
