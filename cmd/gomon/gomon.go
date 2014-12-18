8f0751e6763375b0d31ede0e4b725fc74bd40cdf
Henrik Jonsson <me@hkjn.me>
2014-12-18 01:08:05 +0100

    logs some info in gomon.go

--- cmd/gomon/gomon.go
+++ cmd/gomon/gomon.go
@@ -2,15 +2,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+
+	"github.com/golang/glog"
 
 	"hkjn.me/dashboard"
 )
 
+var bindAddress = ":8080"
+
 func main() {
+	flag.Parse()
+	glog.Infof("gomon version %s initializing..\n", dashboard.Version())
+	glog.Infoln("Starting probes..\n")
 	for _, p := range dashboard.GetProbes() {
 		go p.Run()
 	}
-	log.Fatal(http.ListenAndServe(":8080", dashboard.NewRouter()))
+	glog.Infof("Listening on %s..\n", bindAddress)
+	log.Fatal(http.ListenAndServe(bindAddress, dashboard.NewRouter()))
 }
