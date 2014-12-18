56d922aedee2e5449763f66f6a42d377bde8935a
Henrik Jonsson <me@hkjn.me>
2014-12-18 01:05:59 +0100

    adds version to config.yaml, shows it in index, regenerates bindata

--- index.go
+++ index.go
@@ -9,13 +9,14 @@ import (
 // getIndexData returns the data for the index page.
 func getIndexData(w http.ResponseWriter, r *http.Request) (interface{}, error) {
 	data := struct {
-		ErrorMsg string
-		Links    []struct {
+		Version string
+		Links   []struct {
 			Name, URL string
 		}
 		Probes         []*prober.Probe
 		ProberDisabled bool
 	}{}
+	data.Version = Version()
 	data.Probes = GetProbes()
 	data.ProberDisabled = *proberDisabled
 	return data, nil
