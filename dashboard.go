ce0b13d7212ad1749866891a9e6da865277aa507
Henrik Jonsson <me@hkjn.me>
2017-03-12 15:07:51 +0100

    Test commit

--- dashboard.go
+++ dashboard.go
@@ -56,7 +56,6 @@ type Config struct {
 	EmailRecipient   string
 }
 
-// FIXMEH: blah
 // setProbeCfg sets the config values.
 func setProbesCfg(conf Config, emailTemplate string) error {
 	if conf.Debug {
